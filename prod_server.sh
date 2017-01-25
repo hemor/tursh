bce5fbe757730739a2c05fb4529293f57bdb3ce0
Emmanuel Akinpelu <hemor2709@gmail.com>
2017-01-25 18:57:03 +0100

    Modified prod_server.sh

--- prod_server.sh
+++ prod_server.sh
@@ -1,1 +1,7 @@
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+
+source /usr/local/bin/virtualenvwrapper.sh
+
+workon tursh
+
 python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --no-input && gunicorn -D -w 3 -b 0.0.0.0:8000 config.wsgi:application --reload
