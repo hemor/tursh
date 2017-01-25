6a8b1d353ce3401235aab3ced90d75ede762e544
Emmanuel Akinpelu <hemor2709@gmail.com>
2017-01-25 18:08:01 +0100

    Modified .gitignore

---
+++ config/prod_settings.py
@@ -0,0 +1,22 @@
+from .base_settings import *
+
+
+SECRET_KEY = os.environ.get('TURSH_SK', '');
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('TURSH_DB_ENGINE', ''),
+        'NAME': os.environ.get('TURSH_DB_NAME', ''),
+        'USER': os.environ.get('TURSH_DB_USER', ''),
+        'PASSWORD': os.environ.get('TURSH_DB_PASSWORD', ''),
+        'HOST': os.environ.get('TURSH_DB_HOST', ''),
+        'PORT': os.environ.get('TURSH_DB_PORT', '')
+    }
+}
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', 'tursh.net', 'www.tursh.net']
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
