10bd7b401040996aac192c6e53d61c6543de21f3
Emmanuel Akinpelu <hemor2709@gmail.com>
2017-01-26 14:50:34 +0100

    Added supervior conf

--- prod_server.sh
+++ prod_server.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 
 source /usr/local/bin/virtualenvwrapper.sh
 
 workon tursh
 
-python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --no-input && gunicorn -D -w 3 -b 0.0.0.0:8000 config.wsgi:application --reload
+python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --no-input && gunicorn -D -w 3 -b 0.0.0.0:8000 config.wsgi:application --reload --access-logfile logs/gunicorn_access.log --error-logfile logs/gunicorn_error.log
