81be09eb6d74f3234de0341b9fe442ea4cc1001d
Emmanuel Akinpelu <hemor2709@gmail.com>
2017-01-30 10:32:16 +0100

    Removed supervisor configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tursh
-A simple django app for shortening long urls.
+A simple django app for shortening long urls with 0 delay time.
+
+www.tursh.net
--- prod_server.sh
+++ prod_server.sh
@@ -6,4 +6,6 @@ source /usr/local/bin/virtualenvwrapper.sh
 
 workon tursh
 
+source $HOME/.bashrc
+
 python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic --no-input && gunicorn -D -w 3 -b 0.0.0.0:8000 config.wsgi:application --reload --access-logfile logs/gunicorn_access.log --error-logfile logs/gunicorn_error.log
